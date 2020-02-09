cfd04ac7842b0977170c52b8a3e336888a2d4664
asszxxz <60520528+asszxxz@users.noreply.github.com>
2020-02-09 18:12:30 +0700

    up

---
+++ app/index/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>กรอบมาตรฐานคุณวุฒิระดับอุดมศึกษาแห่งชาติ </title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Cloud 83 - hosting template ">
+	<meta name="keywords" content="cloud, hosting, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon" />
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Poppins:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/font-awesome.min.css" />
+	<link rel="stylesheet" href="css/magnific-popup.css" />
+	<link rel="stylesheet" href="css/owl.carousel.min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<link rel="stylesheet" href="css/animate.css" />
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section">
+		<div class="container">
+			<a href="index.html" class="site-logo">
+				<img src="img/logo.png" alt="logo">
+			</a>
+			<!-- Switch button -->
+			<div class="nav-switch">
+				<div class="ns-bar"></div>
+			</div>
+			<div class="header-right">
+				<ul class="main-menu">
+					<li class="active"><a href="index.html">หน้าแรก</a></li>
+					<li><a href="coures.html">รายวิชา</a></li>
+					<li><a href="service.html">รายวิชาที่เปิดสอน</li>
+					<li><a href="blog.html">มคอ</a></li>
+					<li><a href="contact.html">พิมรายงาน</a></li>
+				</ul>
+				<div class="header-btns">
+					<a href="#" class="site-btn sb-c2">Login</a>
+					<a href="#" class="site-btn sb-c3">Register</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg" data-setbg="img/bg.jpg">
+				<div class="container">
+					<h2>กรอบมาตรฐานคุณวุฒิระดับอุดมศึกษาแห่งชาติ</h2>
+					<div class="clearfix"></div>
+					<a href="coures.html" class="site-btn sb-c1">ไปยังมคอ</a>
+				</div>
+			</div>
+
+
+		</div>
+		<div class="copyright">
+			<p>
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				Copyright &copy;<script>
+					document.write(new Date().getFullYear());
+				</script> All rights reserved | This template is made with <i class="fa fa-heart-o"
+					aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+			</p>
+		</div>
+		</div>
+		</footer>
+		<!-- Footer section end -->
+
+
+		<!--====== Javascripts & Jquery ======-->
+		<script src="js/jquery-3.2.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/owl.carousel.min.js"></script>
+		<script src="js/jquery.magnific-popup.min.js"></script>
+		<script src="js/circle-progress.min.js"></script>
+		<script src="js/main.js"></script>
+
+</body>
+
+</html>
